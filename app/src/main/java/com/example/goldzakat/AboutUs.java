3c049555f1e2a2fba56aca6c3aa39b8e745dc665
AynurFar <135956729+AynurFar@users.noreply.github.com>
2023-12-15 11:45:41 +0800

    GoldZakat

---
+++ app/src/main/java/com/example/goldzakat/AboutUs.java
@@ -0,0 +1,41 @@
+package com.example.goldzakat;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+
+public class AboutUs extends AppCompatActivity {
+
+    public void openUrl(View view) {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/AynurFar/GoldZakat"));
+        startActivity(intent);
+    }
+
+    Toolbar aboutToolbar;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_us);
+
+        aboutToolbar = findViewById(R.id.about_toolbar);
+        setSupportActionBar(aboutToolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
